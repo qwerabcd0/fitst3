12a13957a2c3d4951a5ce357c43eae84fbf0bf76
qwerabcd0 <kin710730@naver.com>
2022-09-23 09:26:00 +0900

    Update hello_world.py

--- hello_world.py
+++ hello_world.py
@@ -1,9 +1,10 @@
-def print_hello():
-    print("Hello World!!")
+def add(x,y):
+    a=x+y
+    return a
 
 def pirint_KIT():
-    print("Hello Kumoh")
+    print("KIT")
 
 if __name__ == '__main__':
-    print_hello()
+    print( print_hello(1,2))
     print_KIT()
